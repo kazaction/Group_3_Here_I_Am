036dfcc865cafcc8b8f64558088a18cedc3e1b8e
Kazaction <ps4psnkikos@gmail.com>
2025-10-21 16:28:39 +0300

    created react + flask

---
+++ here_i_am/src/App.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('/api/hello')
+      .then(res => res.json())
+      .then(data => setMessage(data.message))
+      .catch(err => console.error(err));
+  }, []);
+
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>React + Flask Demo</h1>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default App;
