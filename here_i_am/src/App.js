66ec968bd32854d1a69a89cd7ecb426158b3f479
Giannis <giannisnangelidis@gmail.com>
2025-10-21 16:04:40 +0300

    commiting main file to my branch

---
+++ here_i_am/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+import Hello from './components/hello';
+
+
+
+function App() {
+  return (
+    <div> 
+      <Hello/>
+    </div>
+  );
+}
+
+export default App;
