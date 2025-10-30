3c7e1195e96abaea0357d1693506ed0ce9ef8849
Markos-34 <markosmavroudi@Markoss-MacBook-Pro.local>
2025-10-30 16:10:29 +0200

    New UI for profile and new navbar

--- here_i_am/src/App.js
+++ here_i_am/src/App.js
@@ -4,6 +4,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar';
 import Schedule from './components/schedule';
+import Profile from './components/profile';
 
 
 
@@ -16,6 +17,7 @@ function App() {
         <Routes>
           <Route path="/" element={<h1>Welcome to Here I Am</h1>} />
           <Route path="/schedule" element={<Schedule />} />
+          <Route path="/profile" element={<Profile />} />
           {/* Add more routes like: */}
           {/* <Route path="/history" element={<History />} /> */}
           {/* <Route path="/cv" element={<CVGeneration />} /> */}
