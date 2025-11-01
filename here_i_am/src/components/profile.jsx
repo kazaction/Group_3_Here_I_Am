e57e2ec0e562b6b15dcd7369601233b34be1e04d
Markos-34 <markosmavroudi@Markoss-MacBook-Pro.local>
2025-11-01 23:32:58 +0200

    Add an extra button to verify password first

--- here_i_am/src/components/profile.jsx
+++ here_i_am/src/components/profile.jsx
@@ -10,18 +10,32 @@ const Profile = () => {
     surname: "",
     username: "",
     email: "",
-    password: "",
+    //password: "",
     profile_picture: ""
+    });
+
+    //test password edit
+    const [passwordTest, setProfilePassword] = useState({
+        password: ""
     });
 
 
     //State for edit mode
     const [isEditing, setIsEditing] = useState(false);
 
+    //test for extra edit button
+    const [isEditingPassword, setIsEditingPassword] = useState(false);
+
     //Hadnle input changes
     const handleChange = (e) => {
         const {name, value} = e.target;
         setProfile({ ...profile, [name]: value});
+    };
+
+    //Test password andle inout change
+    const handleChangePassword = (e) => {
+        const {name, value} = e.target;
+        setProfilePassword({ ...passwordTest, [name]: value});
     };
 
     //Handle save (for now just exit mode)
@@ -33,11 +47,29 @@ const Profile = () => {
             .catch(err => console.error(err));
     };
 
+    //Test password handle save
+    const handleSavePassword = () => {
+        setIsEditingPassword(false);
+        axios.put("http://localhost:5000/users/TR_Meowth", passwordTest)
+        .then(res => console.log("Profile updated:",res.data))
+        .catch(err => console.error(err));
+    };
+
     useEffect(() => {
         axios.get("http://localhost:5000/users/TR_Meowth")
             .then(res => setProfile(res.data))
             .catch(err => console.error(err));
     }, []);
+
+    //Test use effect for password
+    useEffect(() => {
+    axios.get("http://localhost:5000/users/TR_Meowth")
+        .then(res => {
+            setProfile(res.data);
+            setProfilePassword({ password: res.data.password || "" }); // <-- populate password state
+        })
+        .catch(err => console.error(err));
+}, []);
 
     return (
         <div>
@@ -59,15 +91,26 @@ const Profile = () => {
             <label>Email Address:</label>
             <input type = "email" name = "email" value={profile.email} onChange={handleChange} readOnly={!isEditing} />
             <br/>
-
-            <label>Password:</label>
+            
+            {/*<label>Password:</label>
             <input type = "password" name = "password" value={profile.password} onChange={handleChange} readOnly={!isEditing} />
-            <br/>
+            <br/>*/}
 
             {!isEditing ? (
             <button onClick={() => setIsEditing(true)}>Edit</button>
             ) : (
                 <button onClick={handleSave}>Save</button>
+            )}
+            <br/>
+
+            <label>Password:</label>
+            <input type = "password" name = "password" value={passwordTest.password} onChange={handleChangePassword} readOnly={!isEditingPassword} />
+            <br/>
+            
+            {!isEditingPassword ? (
+            <button onClick={() => setIsEditingPassword(true)}>Edit</button>
+            ) : (
+                <button onClick={handleSavePassword}>Save</button>
             )}
         </div>
 
