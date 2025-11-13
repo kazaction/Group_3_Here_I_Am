2cc99c00aeedec2418b718bb2fbc6509612f95fb
KouPas <rafaelhadjicharalambous@gmail.com>
2025-11-13 13:03:31 +0200

    This branch is the prototype of main branch. Combned markos and koupas functionalities - working flawless thus far.

---
+++ here_i_am/src/components/home.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+const Home = () => {
+    return(
+        <h1>Welcome to home page</h1>
+    );
+};
+
+  export default Home;
