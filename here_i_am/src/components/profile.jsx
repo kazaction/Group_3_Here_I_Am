b2e3ce74262d8e4f1a3bad9db20974dca3239b73
Markos-34 <markosmavroudi11@gmail.com>
2025-11-19 12:36:23 +0200

    added the userid that takes from the log in

--- here_i_am/src/components/profile.jsx
+++ here_i_am/src/components/profile.jsx
@@ -11,7 +11,7 @@ const Profile = () => {
 
     const navigate = useNavigate(); 
 
-    const userId = 1; // TEMP: hardcoded until login page exists
+    const userObj = JSON.parse(localStorage.getItem("user") || '{}');
     const apiBase = `http://localhost:3001/users/${userId}`;
     
     const [profile, setProfile] = useState({
