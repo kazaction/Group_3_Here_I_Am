7d3eabbc74269d55b9134ba8e63b049c27e4f34a
kazaction <ps4psnkikos@gmail.com>
2025-12-02 15:34:07 +0200

    created upload services

---
+++ here_i_am/backend/upload_services.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+def save_file(file_storage, user_id, event_id):
+    """
+    file_storage = request.files["file"]
+    """
+    filename = file_storage.filename
+    file_data = file_storage.read()
+
+    # Save to database
+    conn = sqlite3.connect("./db/database.db")
+    cursor = conn.cursor()
+    cursor.execute(
+        "INSERT INTO uploads (filename, filedata, user_id, event_id) VALUES (?, ?)",
+        (filename, file_data, user_id, event_id)
+    )
+    conn.commit()
+    conn.close()
+
+    print(f"File saved successfully: {filename}")
+    return filename
